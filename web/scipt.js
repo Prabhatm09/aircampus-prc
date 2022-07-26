b86c2b7dcc4adc76127b2efb328687ecff1fd7c4
prabhatm09 <prabhatmishra1409@gmail.com>
2022-07-26 20:04:41 +0530

    clone

--- web/scipt.js
+++ web/scipt.js
@@ -1,6 +1,11 @@
 let button = document.querySelector(".mybutton");
 let div = document.querySelector("#list");
+let button1 = document.querySelector(".mybutton1")
+let button2 = document.querySelector(".mybutton2")
+let div1 = document.querySelector("#btn");
 button.addEventListener("click", Myfunction);
+button1.addEventListener("click", myFnt);
+button2.addEventListener("click", myFnt2);
     // button.addEventListener("click", Myfunction2);
     function Myfunction(){
         if(div.style.display == "none"){
@@ -8,4 +13,11 @@ button.addEventListener("click", Myfunction);
         }else {
             div.style.display = "none"
         }
+    }
+
+    function myFnt(){
+        div1.style.display ="block"
+    }
+    function myFnt2(){
+        div1.style.display ="none"
     }
