90c2ee5970e3da4abf2ba92af52e1d81073a9124
prabhatm09 <prabhatmishra1409@gmail.com>
2022-08-03 14:12:05 +0530

    dynamic table

---
+++ dynamic table/index.js
@@ -0,0 +1,28 @@
+let button = document.querySelector('#sucess');
+let button1 = document.querySelector('table')
+
+button.addEventListener('click' , ()=> {
+  let name = document.querySelector('#name').value;
+    let gmail = document.querySelector('#gmail').value;
+    let number = document.querySelector('#number').value;
+    
+    if(!name || !gmail || !number){
+        alert('please fill  all the boxes')
+        return;
+    }
+    let display = document.querySelector('#display');
+    display.innerHTML += `<tr>
+    <th>${name}</th>
+    <th>${gmail}</th>
+    <th>${number}</th>
+    <th><button class="danger">Remove</button></th>
+    </tr>`
+    
+    
+    
+  })
+
+  button1.addEventListener('click', (e)=>{
+    e.target.closest('tr').remove();
+  })
+  
