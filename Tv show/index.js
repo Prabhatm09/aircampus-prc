bded7875a8e0ed2efdb460e7f76dad2dfbc6d122
prabhatm09 <prabhatmishra1409@gmail.com>
2022-09-01 14:44:17 +0530

    tv show

--- Tv show/index.js
+++ Tv show/index.js
@@ -1,69 +1,49 @@
 const input = document.querySelector(".input");
-const button = document.querySelector(".btn");
+const button = document.querySelector(".search");
+// const container = document.querySelector(".container");
 
 
 
 
 button.addEventListener("click", getdata);
 
-function getdata(e) {
-    const apiUrl = `https://api.tvmaze.com/search/shows?q=${input.value}`
-    fetch(apiUrl)
-        .then(res => res.json())
-        .then((data) => {
+function getdata() {
+  const apiUrl = `https://api.tvmaze.com/search/shows?q=${input.value}`
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then((data) => {
 
-            getinfo(data)
+      getinfo(data)
 
-            console.log(data);
-        })
+      // console.log(data);
+    })
 }
 
 function getinfo(info) {
 
-    info.forEach(element => {
-        let list = `<div class="details">
-      <img src="${element.show.image.original}" alt="img" class="img">
-      <h1 class="h1">NAME : ${element.show.name}</h1>
-      <h2 class="h2">STARTS :${element.show.premiered}</h2>
-      <h2 class="h2">LANGUAGE : ${element.show.language}</h2>
+  for (let i = 0; i < info.length; i++) {
+    let list = `<div class="details">
+      <img src="${info[i].show.image.medium}" alt="img" class="img">
+      <h1 class="h1">NAME : ${info[i].show.name}</h1>
+      <h2 class="h2">STARTS :${info[i].show.premiered}</h2>
+      <h2 class="h2">LANGUAGE : ${info[i].show.language}</h2>
       <button class="showbtn">
-      <a href="${element.show.url}" target="_blank" id="links">Show Details</a></button>
+      <a href="./show.html"  id="${info[i].score}">Show Details</a></button>
       </div>`
-      
-      
+
       let container = document.querySelector(".container")
-      container.innerHTML += list
-    }) 
+    container.innerHTML += list
+
+    container.addEventListener("click", (e) => {
+      if (e.target.id == info[i].score) {
+
+        let showdata = info[i]
+        // console.log(showdata);
+        sessionStorage.setItem("key", JSON.stringify(showdata));
+      }
+    })
+  }
 }
-
- const showdetail = document.querySelector(".showbtn")
-    showdetail.addEventListener("click", () => {
-         fetch(`https://api.tvmaze.com/shows/1?embed[]=seasons&embed[]=cast`)
-         .then(res => res.json())
-         .then((details =>{
-               getNewpageDetails(details);
-               console.log(details);
-         }))
-    });
-
-
-    function getNewpageDetails(data){
-        data.forEach(element => {
-            let list = `<div class="data">
-          <img src="${element.show.image.original}" alt="img" class="img">
-          <h1 class="h1">NAME : ${element.name}</h1>
-          <h2 class="h2">STARTS :${element.show.premiered}</h2>
-          <h2 class="h2">LANGUAGE : ${element.show.language}</h2>
-          <h2 class="h2"> ${element.show.language}</h2>
-         
-          </div>`
-
-          let container = document.querySelector(".container")
-      container.innerHTML = list
-        })
-    }
-
-
 
 
 
